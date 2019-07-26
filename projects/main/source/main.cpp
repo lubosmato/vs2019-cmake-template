a0175ad55e4350b885e108c86d17aee59a44c5b2
Luboš Matejčík <lubos.matejcik@tescan.com>
2019-07-26 21:40:21 +0200

    Add external dependency to OpenCV

--- projects/main/source/main.cpp
+++ projects/main/source/main.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
 #include <boost/asio.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
 #include "huge_library/greeting.hpp"
 #include "dll_library/greeting.hpp"
 
@@ -15,12 +19,19 @@ int main()
 
 	boost::asio::io_context io_context;
 	boost::asio::post(io_context, my_task);
-	boost::asio::post(io_context, []() {
-		std::cout << "What is doing this?\n";
-		});
-
-	// Run the io_context until it runs out of work.
+	boost::asio::post(
+		io_context, 
+		[]() {
+			std::cout << "What is doing this?\n";
+		}
+	);
 	io_context.run();
+
+	cv::Mat image = cv::Mat(256, 256, CV_8UC3, cv::Scalar{ 0 });
+	cv::line(image, { 0, 0 }, { 255, 255 }, { 80, 160, 240 });
+	cv::imshow("Image", image);
+	cv::waitKey();
+	cv::destroyAllWindows();
 
 	return 0;
 }
