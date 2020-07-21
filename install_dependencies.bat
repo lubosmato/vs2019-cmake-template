b7ed523576d97ad6a4559ac5421bfb1ff532f72b
Luboš Matejčík <lubos.matejcik@tescan.com>
2020-07-21 16:33:15 +0200

    Update dependency installer

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,4 +1,5 @@
 @echo OFF
 rmdir /q /s build
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan install . -if ./build/ -g cmake_multi -s build_type=Debug --build missing
 conan install . -if ./build/ -g cmake_multi -s build_type=Release --build missing
